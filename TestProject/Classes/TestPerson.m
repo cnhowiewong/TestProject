831ba470ebc9bdf29f70296e4f57e26f3a1f79c7
cnhowiewong <howie0307why@163.com>
2021-06-21 14:44:29 +0800

    feat

---
+++ TestProject/Classes/TestPerson.m
@@ -0,0 +1,16 @@
+//
+//  TestPerson.m
+//  TestProject
+//
+//  Created by 王厚一 on 2021/6/21.
+//
+
+#import "TestPerson.h"
+
+@implementation TestPerson
+
+- (void)foo {
+    NSLog(@"function foo");
+}
+
+@end
